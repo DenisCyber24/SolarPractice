1174ef4669c842b41e71badd8ee3becfce40743a
DenisCyber24 <bjelopolje@hotmail.com>
2024-11-23 12:03:44 -0500

    solar system basic
    pwd
    is ready

---
+++ src/test/java/SolarS.java
@@ -0,0 +1,25 @@
+public class SolarS {
+    public static void main(String[] args) {
+
+       BasicStructure BB=new BasicStructure();
+       BB.Planets="2";
+       BB.Moon="2";
+       BB.Stars="10";
+       BB.Sun="1";
+        System.out.println("Solar is ready");
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
