546cf800012ee528c164ea28252c5591434f796a
DenisCyber24 <bjelopolje@hotmail.com>
2024-11-24 19:37:02 -0500

    featuresun added

---
+++ src/test/java/FeatureSun.java
@@ -0,0 +1,11 @@
+package test.java;
+
+public class FeatureSun {
+
+        String Color;
+        String tem;
+        String Radius;
+
+
+    }
+
---
+++ src/test/java/SolarSys.java
@@ -0,0 +1,20 @@
+package test.java;
+
+public class SolarSys {
+        public static void main(String[] args) {
+
+            BasicStructure BB=new BasicStructure();
+            BB.Planets="2";
+            BB.Moon="2";
+            BB.Stars="10";
+            BB.Sun="1";
+            System.out.println("Solar is ready");
+
+            FeatureSun Xros=new FeatureSun();
+            Xros.tem="324325kd";
+            Xros.Color="green";
+            Xros.Radius="6458593754759";
+
+
+        }
+}
